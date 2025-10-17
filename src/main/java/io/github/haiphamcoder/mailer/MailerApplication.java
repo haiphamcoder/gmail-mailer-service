dc81bad3ab673e829cc25bc8ddb0b9690e1e9de2
Hai Pham Ngoc <ngochai285nd@gmail.com>
2025-10-17 16:52:37 +0700

    init project

---
+++ src/main/java/io/github/haiphamcoder/mailer/MailerApplication.java
@@ -0,0 +1,13 @@
+package io.github.haiphamcoder.mailer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MailerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/haiphamcoder/mailer/MailerApplicationTests.java
@@ -0,0 +1,16 @@
+package io.github.haiphamcoder.mailer;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailerApplicationTests {
+
+	@Test
+	void contextLoads() {
+		assertTrue(true);
+	}
+
+}
